621ac3faea8d153c4eb6536f7fe1c4962b9d8b73
cccwon2 <cccwon2@naver.com>
2024-09-21 02:48:10 +0900

    swagger 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SwaggerModule } from '@nestjs/swagger';
 
 @Module({
-  imports: [],
+  imports: [SwaggerModule],
   controllers: [AppController],
   providers: [AppService],
 })
