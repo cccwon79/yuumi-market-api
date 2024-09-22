c762d9497a95f7a8f07ed404169f60f9f0d4ea1f
cccwon2 <cccwon2@naver.com>
2024-09-22 10:22:58 +0900

    chore: 메인 페이지에 Swagger 링크 추가

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "Welcome to Panda Market API!";
+    return "<a href='/docs'> Welcome to Yuumi Market API</a>";
   }
 }
