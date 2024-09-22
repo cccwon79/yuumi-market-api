440ca3cca722f9a585157fbd24167e3302471e72
cccwon2 <cccwon2@naver.com>
2024-09-22 09:32:53 +0900

    chore: REAMME.md 파일 수정, Swagger api 에서 docs 로 수정

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
+  SwaggerModule.setup("docs", app, document);
 
   app.enableCors();
   app.useGlobalGuards(new JwtAuthGuard());
